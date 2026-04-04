f86de54d6d7fdae024024dddf9088326e7df8d5e
Atakan Akdeniz <at.akdnz@gmail.com>
2026-04-05 01:17:45 +0300

    Remove build outputs from tracking, fix gitignore
    Add app/build/ pattern to gitignore and untrack all generated build files

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_vaultmind_app_VaultMindApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.vaultmind.app.VaultMindApp"
-)
-public final class _com_vaultmind_app_VaultMindApp {
-}
